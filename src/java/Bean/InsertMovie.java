c07c7e45b489fed5a92b4b924b0b7573ea1d4e43
Arnold <Arnold@Arnold-PC>
2017-06-07 23:22:13 +0200

    Stand 07.06 Abend

--- src/java/Bean/InsertMovie.java
+++ src/java/Bean/InsertMovie.java
@@ -7,12 +7,17 @@ package Bean;
  */
 
 import Control.Controller;
+import Control.DBConnection;
 import Control.Movie;
 import Control.MovieRegisseur;
 import Control.Regisseur;
+import Entity.Eactor;
+import Entity.Egenre;
 import java.io.Serializable;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
@@ -25,18 +30,23 @@ import javax.inject.Named;
 @SessionScoped
 
 public class InsertMovie implements Serializable {
+    private DBConnection DBC = new DBConnection();
     private String title;
     private int releaseDate;
     private String regisseur;
+    private ArrayList<String> actor;
 
-
-    
+    private ArrayList<ArrayList<String>> actorList;
     private ArrayList<Genre> genreList;
+    
+    
+    private String debugMessage="";
     
     /**
      * Creates a new instance of InsertMovie
      */
     public InsertMovie() {
+        
     }
 
     public String getTitle() {
@@ -100,11 +110,64 @@ public class InsertMovie implements Serializable {
     }
     
    
+    public ArrayList<String> getActorList() {
+        return actorList;
+    }
+
+    
+    public void setActorList(ArrayList<String> actorList) {
+        this.actorList = actorList;
+    }
+    
+    public ArrayList<String> getActor() {
+        return actor;
+    }
+
+    public void setActor(ArrayList<String> actor) {
+        
+            this.actor = actor;    
+        
+    }
+    
+    
     
     public void addGenreToList(String genreName){
         genreList.add(new Genre(genreName));
     }
+    
+    @PostConstruct
+    public void init(){
+        if(actorList == null){
+            actorList = new ArrayList<>();    
+        }
+        ResultSet rs;
+        try {
+            
+                rs = DBC.getRS("SELECT actorName FROM actor");
+                if(rs.isBeforeFirst()){
+                }
+                while(rs.next()){
+        
+                    String actorName = rs.getString("actorName");
+                    actorList.add(actorName);
+                }
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
     public boolean checkLoginValidation() throws SQLException {
         return CheckLogin.checkLoginValidation();
     }
+
+    public String getDebugMessage() {
+        return debugMessage;
+    }
+
+    public void setDebugMessage(String debugMessage) {
+        this.debugMessage = debugMessage;
+    }
+    
+    
 }
